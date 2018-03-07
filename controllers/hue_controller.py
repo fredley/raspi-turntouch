3f63044e9ed2ffe83ef04c322734e5f7bec4fdc9
Tom White <fredley@gmail.com>
2018-03-07 18:47:10 +0000

    New controllers

--- controllers/hue_controller.py
+++ controllers/hue_controller.py
@@ -1,13 +1,13 @@
-import logging
 import json
 from qhue import Bridge, create_new_username
 import requests
+import sys
 
-logger = logging.getLogger('hue_controller')
+from .base_controller import BaseController
 
-class HueController:
+class HueController(BaseController):
 
-    def __init__(self):
+    def init(self):
         try:
             with open('.hueusername') as f:
                 bridge_data = json.loads(f.read())
@@ -20,24 +20,54 @@ class HueController:
             with open('.hueusername', 'w') as f:
                 f.write(json.dumps(bridge_data))
         self.bridge = Bridge(bridge_data['internalipaddress'], bridge_data['username'])
-        logger.info("Successfully connected to Hue Bridge {}".format(bridge_data['internalipaddress']))
+        self.log("Successfully connected to Hue Bridge {}".format(bridge_data['internalipaddress']))
 
-    def print_all_lights(self):
-        rooms = self.bridge.groups()
-        for id, light in self.bridge.lights().items():
-            room = [r for r in rooms.values() if id in r['lights']]
-            logger.info("[{}]: {} ({}){}".format(
-                id, light['name'], light['type'],
-                " in {}".format(room[0]['name']) if room else ""))
+    def print_all(self):
+        self.log("Lights:")
+        for room_id, room in self.bridge.groups().items():
+            self.log("{} [ID: {}] - {}:".format(room['type'], room_id, room['name']))
+            for light_id in room['lights']:
+                light = self.bridge.lights()[light_id]
+                self.log(" - [ID: {}]: {} ({})".format(
+                    light_id, light['name'], light['type']))
+            self.log(" ")
+        self.log("Scenes:")
+        for scene in self.bridge.scenes().values():
+            self.log(" - {}".format(scene['name']))
 
     def set_light(self, id, *args, **kwargs):
+        self.log("Setting light {}: {}".format(id, kwargs))
         self.bridge.lights[id].state(**kwargs)
 
+    def set_room(self, id, *args, **kwargs):
+        self.log("Setting room {}: {}".format(id, kwargs))
+        self.bridge.groups[id].state(**kwargs)
+
+    def adjust_light_brightness(self, id, *args, **kwargs):
+        current_amount = self.bridge.lights[id]['brightness']
+        if kwargs['direction'] == 'up':
+            new_amount = current_amount + kwargs.get('amount', 16)
+        else:
+            new_amount = current_amount - kwargs.get('amount', 16)
+        self.set_light(id, **{'brightness': new_amount})
+
+    def set_scene(self, id, *args, **kwargs):
+        scene_id = [k for k, v in self.bridge.scenes().items() if v['name'] == kwargs['scene']]
+        self.bridge.groups[id].state({'scene': scene_id})
+
     def perform(self, action):
-        self.set_light(action['id'], bri=action['brightness'], hue=action['hue'])
+        id, action = action['id'], action['action']
+        kwargs = {k: v for k, v in action.items() if k not in ['action', 'id']}
+        if action == 'set_light':
+            self.set_light(id, **kwargs)
+        elif action == 'set_room':
+            self.set_room(id, **kwargs)
+        elif action == 'adjust_brightness':
+            pass
+        elif action == 'set_scene':
+            self.set_scene(id, **kwargs)
 
 
 if __name__ == '__main__':
-    logger.addHandler(logging.StreamHandler())
-    c = HueController()
-    c.print_all_lights()
+    c = HueController(print=True)
+    c.print_all()
